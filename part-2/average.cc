2419a2654bdd9bbcaab221ee5b85571c766759e9
chetachiihetu <143053466+chetachiihetu@users.noreply.github.com>
2023-11-03 01:31:30 +0000

    fixed all formatting and linting errors.

--- part-2/average.cc
+++ part-2/average.cc
@@ -9,17 +9,23 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-  if (arguments.size() < 2) {
-    std::cout << "error: you must supply at least one number";
-    return 1;
+  if (arguments.size() < 2 || arguments.empty()) {
+    std::cout << "Error: you must supply at least one argument\n";
+    return 2;
   }
-  double numb = std::stod(arguments.size()) - 1;
-  double total{0};
-  for (int i = 1; i < numb + 1; ++i) {
-    double add = std::stod(arguments[i]);
-    total += add;
+  double sum{0.0};
+  bool first = true;
+  for (const std::string& arg : arguments) {
+    if (first) {
+      first = false;
+    } else {
+      double number = std::stod(arg, nullptr);
+      sum += number;
+    }
   }
-  double average{total / numb};
-  std::cout << "average = " << average << '\n';
+  double avg{sum / static_cast<double>(arguments.size() - 1)};
+
+  std::cout << "average = " << avg << "\n";
+
   return 0;
 }
